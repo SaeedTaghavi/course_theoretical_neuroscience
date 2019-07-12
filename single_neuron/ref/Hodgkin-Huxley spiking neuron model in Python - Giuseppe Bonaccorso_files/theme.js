b961da805b10036eacb551f215cd5924e7e5c3d2
Saeed Taghavi <saeed.taghavi.v@gmail.com>
2019-07-12 10:40:02 +0430

    single neuron files sorted

--- single_neuron/ref/Hodgkin-Huxley spiking neuron model in Python - Giuseppe Bonaccorso_files/theme.js
+++
@@ -1,4 +0,0 @@
-jQuery.noConflict()(function($){$(document).ready(function(){function backToTopButton(){var
-$scrollTopBtn=$('#back_top');if($scrollTopBtn.hasClass('visible-button')){$scrollTopBtn.removeClass('visible-button');}
-$(window).scroll(function(){if($(window).scrollTop()>0){$scrollTopBtn.addClass('visible-button');}else{$scrollTopBtn.removeClass('visible-button');}});$scrollTopBtn.on('click',function(){$('html:not(:animated), body:not(:animated)').animate({scrollTop:0},0);return false;});}
-backToTopButton();});});
